2dfa95a12ce9c6c24e4982ea12fd443987295702
alex cherepanov <a.cherepanov@olimp.kz>
2020-09-23 23:18:02 +0600

    change var to const

--- js/stat.js
+++ js/stat.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_PADDING = 20;
-var GAP = 10;
-var WELCOME_TEXT_X = CLOUD_X + CLOUD_PADDING;
-var WELCOME_TEXT_Y = CLOUD_Y + CLOUD_PADDING;
-var CHART_X = CLOUD_X + CLOUD_PADDING * 2;
-var CHART_Y = CLOUD_Y + 80;
-var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
-var BAR_GAP = 50;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_PADDING = 20;
+const GAP = 10;
+const WELCOME_TEXT_X = CLOUD_X + CLOUD_PADDING;
+const WELCOME_TEXT_Y = CLOUD_Y + CLOUD_PADDING;
+const CHART_X = CLOUD_X + CLOUD_PADDING * 2;
+const CHART_Y = CLOUD_Y + 80;
+const BAR_WIDTH = 40;
+const BAR_HEIGHT = 150;
+const BAR_GAP = 50;
 
 // Функция отрисовки окна с результатами
-var renderCloud = function(ctx, x, y, color) {
+const renderCloud = function(ctx, x, y, color) {
     ctx.font = '16px PT Mono';
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 // Функция отрисовки текста
-var renderText = function(ctx, x, y, text, baseline) {
+const renderText = function(ctx, x, y, text, baseline) {
     ctx.fillStyle = '#000';
     ctx.textBaseline = baseline;
     ctx.fillText(text, x, y);
 };
 
 // Функция которая находит лучший результат
-var getMaxElement = function(times) {
-    var maxElement = times[0];
-    for (var i = 0; i < times.length; i++) {
+const getMaxElement = function(times) {
+    const maxElement = times[0];
+    for (let i = 0; i < times.length; i++) {
         if (times[i] > maxElement) {
             maxElement = times[i];
         }
@@ -40,8 +40,8 @@ var getMaxElement = function(times) {
 };
 
 // Получаем цвет бара
-var getBarColor = function(name) {
-    var barColor = 'rgba(2, 14, 134, ' + Math.random() + ')';
+const getBarColor = function(name) {
+    const barColor = 'rgba(2, 14, 134, ' + Math.random() + ')';
 
     if (name === 'Вы') {
         barColor = 'rgba(255, 0, 0, 1)';
@@ -59,13 +59,13 @@ window.renderStatistics = function(ctx, names, times) {
     renderText(ctx, WELCOME_TEXT_X, WELCOME_TEXT_Y + GAP * 2, 'Список результатов:', 'hanging');
 
     // Получаем лучший результат
-    var maxTime = getMaxElement(times);
+    const maxTime = getMaxElement(times);
 
     // Рисуем гистограмму
-    for (var i = 0; i < names.length; i++) {
-        var currentBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
-        var coordX = CHART_X + (BAR_WIDTH + BAR_GAP) * i;
-        var coordY = CHART_Y + (BAR_HEIGHT - currentBarHeight);
+    for (let i = 0; i < names.length; i++) {
+        const currentBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
+        const coordX = CHART_X + (BAR_WIDTH + BAR_GAP) * i;
+        const coordY = CHART_Y + (BAR_HEIGHT - currentBarHeight);
 
         renderText(ctx, coordX, coordY, Math.floor(times[i]), 'bottom');
         renderText(ctx, coordX, CHART_Y + BAR_HEIGHT + GAP, names[i], 'hanging');
