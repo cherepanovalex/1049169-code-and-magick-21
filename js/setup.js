ba1d431143fb240ccb1a44e3c23ec279ac3a6a59
alex cherepanov <a.cherepanov@olimp.kz>
2020-10-19 17:08:45 +0600

    get js files

--- js/setup.js
+++ js/setup.js
@@ -1,78 +1,76 @@
 'use strict';
 
-(function () {
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+(function() {
+    const COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+    const EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+    const FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-  var setup = document.querySelector('.setup');
-  var setupWizardForm = setup.querySelector('.setup-wizard-form');
-  var wizardEyes = setupWizardForm.querySelector('.wizard-eyes');
-  var wizardCoat = setupWizardForm.querySelector('.wizard-coat');
-  var fireballWrap = setupWizardForm.querySelector('.setup-fireball-wrap');
+    const setup = document.querySelector('.setup');
+    const setupWizardForm = setup.querySelector('.setup-wizard-form');
+    const wizardEyes = setupWizardForm.querySelector('.wizard-eyes');
+    const wizardCoat = setupWizardForm.querySelector('.wizard-coat');
+    const fireballWrap = setupWizardForm.querySelector('.setup-fireball-wrap');
 
-  window.setup = {
-    // Изменение цвета глаз при клике
-    onWizardEyesClick: function () {
-      var eyesColor = window.util.getRandomItemFromArray(EYES_COLORS);
-      wizardEyes.style.fill = eyesColor;
-      setupWizardForm.querySelector('input[name=eyes-color]').value = eyesColor;
-    },
-    // Изменение цвета fireball при клике
-    onFireballClick: function () {
-      var fireballColor = window.util.getRandomItemFromArray(FIREBALL_COLORS);
-      fireballWrap.style.backgroundColor = fireballColor;
-      fireballWrap.querySelector('input').value = fireballColor;
-    },
-    // Изменение цвета плаща при клике
-    onWizardCoatClick: function () {
-      var coatColor = window.util.getRandomItemFromArray(COAT_COLORS);
-      wizardCoat.style.fill = coatColor;
-      setupWizardForm.querySelector('input[name=coat-color]').value = coatColor;
-    },
-    // Перемещение артефакта
-    onShopElementDragstart: function (dragstartEvt) {
-      var artifactsElement = document.querySelector('.setup-artifacts');
-      var draggedItem = null;
+    window.setup = {
+        // Изменение цвета глаз при клике
+        onWizardEyesClick: function() {
+            const eyesColor = window.util.getRandomItemFromArray(EYES_COLORS);
+            wizardEyes.style.fill = eyesColor;
+            setupWizardForm.querySelector('input[name=eyes-color]').value = eyesColor;
+        },
+        // Изменение цвета fireball при клике
+        onFireballClick: function() {
+            const fireballColor = window.util.getRandomItemFromArray(FIREBALL_COLORS);
+            fireballWrap.style.backgroundColor = fireballColor;
+            fireballWrap.querySelector('input').value = fireballColor;
+        },
+        // Изменение цвета плаща при клике
+        onWizardCoatClick: function() {
+            const coatColor = window.util.getRandomItemFromArray(COAT_COLORS);
+            wizardCoat.style.fill = coatColor;
+            setupWizardForm.querySelector('input[name=coat-color]').value = coatColor;
+        },
+        // Перемещение артефакта
+        onShopElementDragstart: function(dragstartEvt) {
+            const artifactsElement = document.querySelector('.setup-artifacts');
+            const draggedItem = null;
 
-      if (dragstartEvt.target.tagName.toLowerCase() === 'img') {
-        draggedItem = dragstartEvt.target;
-        dragstartEvt.dataTransfer.setData('text/plain', dragstartEvt.target.alt);
-        artifactsElement.style.outline = '2px dashed red';
-      }
+            if (dragstartEvt.target.tagName.toLowerCase() === 'img') {
+                draggedItem = dragstartEvt.target;
+                dragstartEvt.dataTransfer.setData('text/plain', dragstartEvt.target.alt);
+                artifactsElement.style.outline = '2px dashed red';
+            }
 
-      artifactsElement.addEventListener('dragstart', function () {
-        artifactsElement.style.outline = '2px dashed red';
-      });
+            artifactsElement.addEventListener('dragstart', function() {
+                artifactsElement.style.outline = '2px dashed red';
+            });
 
-      artifactsElement.addEventListener('dragover', function (evt) {
-        evt.preventDefault();
-        return false;
-      });
+            artifactsElement.addEventListener('dragover', function(evt) {
+                evt.preventDefault();
+                return false;
+            });
 
-      artifactsElement.addEventListener('drop', function (evt) {
-        evt.preventDefault();
-        evt.target.style.backgroundColor = '';
-        evt.target.appendChild(draggedItem);
-        artifactsElement.style.outline = '';
-      });
+            artifactsElement.addEventListener('drop', function(evt) {
+                evt.preventDefault();
+                evt.target.style.backgroundColor = '';
+                evt.target.appendChild(draggedItem);
+                artifactsElement.style.outline = '';
+            });
 
-      artifactsElement.addEventListener('dragenter', function (evt) {
-        evt.preventDefault();
-        evt.target.style.backgroundColor = 'yellow';
-      });
+            artifactsElement.addEventListener('dragenter', function(evt) {
+                evt.preventDefault();
+                evt.target.style.backgroundColor = 'yellow';
+            });
 
-      artifactsElement.addEventListener('dragleave', function (evt) {
-        evt.preventDefault();
-        evt.target.style.backgroundColor = '';
-      });
+            artifactsElement.addEventListener('dragleave', function(evt) {
+                evt.preventDefault();
+                evt.target.style.backgroundColor = '';
+            });
 
-      document.addEventListener('dragend', function (evt) {
-        evt.preventDefault();
-        artifactsElement.style.outline = '';
-      });
-    }
-  };
+            document.addEventListener('dragend', function(evt) {
+                evt.preventDefault();
+                artifactsElement.style.outline = '';
+            });
+        }
+    };
 })();
-
-
--- js/util.js
+++ js/util.js
@@ -1,11 +1,11 @@
 'use strict';
 
-(function () {
-  window.util = {
-    // Получаем случайные данные из массива
-    getRandomItemFromArray: function (array) {
-      var randomItem = array[Math.floor(Math.random() * array.length)];
-      return randomItem;
-    }
-  };
+(function() {
+    window.util = {
+        // Получаем случайные данные из массива
+        getRandomItemFromArray: function(array) {
+            var randomItem = array[Math.floor(Math.random() * array.length)];
+            return randomItem;
+        }
+    };
 })();
